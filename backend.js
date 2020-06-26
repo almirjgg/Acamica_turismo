111bff15414e4f53f18dd8fee73a544ee175c06e
Matybarite <61297628+Matybarite@users.noreply.github.com>
2020-06-25 21:06:54 -0300

    array agregado

--- backend.js
+++ backend.js
@@ -2,6 +2,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const paquetes = [
+    {
+        id: 1,
+        imagen: img1,
+        descripcion: "Si queres relajarte, este es el lugar",
+        titulo: "Punta Cana",
+        precio: "USD 2000"
+    },
+    {
+        id: 2,
+        imagen: img2,
+        descripcion: "Si te gusta la aventura, no te podes perder estas montanas",
+        titulo: "San Martin de los Andes",
+        Precio: "USD 1200"
+    },
+    {
+        id: 3,
+        imagen: img3,
+        descripcion: "Vola bajito en las pistas de Sky",
+        titulo: "Las Lenas",
+        precio: "USD 1700"
+    },
+    {
+        id: 4,
+        imagen: img4,
+        descripcion: "Sorprendete con la ciudad mas austral",
+        titulo: "Ushuaia",
+        precio: "USD 1500"
+    },
+    {
+        id: 5,
+        imagen: img4,
+        descripcion: "Una de las maravillas del mundo",
+        titulo: "Cataratas del Iguazu",
+        precio: "USD 1000"
+    }
+];
 
 app.listen(port, (req, res) => {
     console.log('corriendo servidor');
