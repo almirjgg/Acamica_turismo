51de8fbf47ecc1f9a3414962a20388a4484e33ba
Almirguns <almir.garcia@qubiter.com.ar>
2020-06-29 19:55:46 -0300

    array de packs con imagenes

--- packs.js
+++ packs.js
@@ -2,48 +2,46 @@ const { Router } = require('express');
 const router = Router();
 
 
-const packs = [{
-        id: 1,
-        imagen: 'img1',
-        descripcion: "Si queres relajarte, este es el lugar",
-        titulo: "Punta Cana",
-        precio: "USD 2000",
-        vendido: false
+const packs = [
+    {
+      id: 1,
+      imagen:
+        "https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000/itemimages/88/27/882789_v7.jpeg",
+      descripcion: "Si queres relajarte, este es el lugar",
+      titulo: "Punta Cana",
+      precio: "USD 2000",
     },
     {
-        id: 2,
-        imagen: 'img2',
-        descripcion: "Si te gusta la aventura, no te podes perder estas montanas",
-        titulo: "San Martin de los Andes",
-        Precio: "USD 1200",
-        vendido: false
+      id: 2,
+      imagen:
+        "https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_1300,q_auto,w_2000//uploadimages/23/20/23208666.jpeg",
+      descripcion: "Si te gusta la aventura, no te podes perder estas montanas",
+      titulo: "San Martin de los Andes",
+      Precio: "USD 1200",
     },
     {
-        id: 3,
-        imagen: 'img3',
-        descripcion: "Vola bajito en las pistas de Sky",
-        titulo: "Las Lenas",
-        precio: "USD 1700",
-        vendido: false
+      id: 3,
+      imagen: "https://bucket1.glanacion.com/anexos/fotos/01/2721801.jpg",
+      descripcion: "Vola bajito en las pistas de Sky",
+      titulo: "Las Leñas",
+      precio: "USD 1700",
     },
     {
-        id: 4,
-        imagen: 'img4',
-        descripcion: "Sorprendete con la ciudad mas austral",
-        titulo: "Ushuaia",
-        precio: "USD 1500",
-        vendido: false
+      id: 4,
+      imagen: "https://madryn.org/wp-content/uploads/2017/02/ushuaia.jpg",
+      descripcion: "Sorprendete con la ciudad mas austral",
+      titulo: "Ushuaia",
+      precio: "USD 1500",
     },
     {
-        id: 5,
-        imagen: 'img4',
-        descripcion: "Una de las maravillas del mundo",
-        titulo: "Cataratas del Iguazu",
-        precio: "USD 1000",
-        vendido: false
-    }
-];
-
+      id: 5,
+      imagen:
+        "https://upload.wikimedia.org/wikipedia/commons/9/97/Foz_de_Igua%C3%A7u_27_Panorama_Nov_2005.jpg",
+      descripcion: "Una de las maravillas del mundo",
+      titulo: "Cataratas del Iguazu",
+      precio: "USD 1000",
+    },
+  ];
 
 /* Middleware */
 
